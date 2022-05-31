58372fc8e09f16bd14e90f53c90f76a63d84e12d
Kelvin Ubaechu <kubaechu@gmail.com>
2022-05-30 20:57:05 -0400

    Implement ctof function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -2,8 +2,8 @@ const ftoc = function(temp) {
   return +((temp - 32) * (5/9)).toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  return +(temp * 1.8 + 32).toFixed(1);
 };
 
 // Do not edit below this line
