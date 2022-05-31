6e7fca6a919d704756daab6ddc240a78c01a2901
Kelvin Ubaechu <kubaechu@gmail.com>
2022-05-30 20:28:12 -0400

    Implement sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if(typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'ERROR';
+    }
+    let start, end;
+    if(num1 < num2) {
+        start = num1;
+        end = num2;
+    } else {
+        start = num2;
+        end = num1;
+    }
+    let sum = 0;
+    for(let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
