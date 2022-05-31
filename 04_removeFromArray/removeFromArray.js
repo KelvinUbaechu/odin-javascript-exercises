b4949a2f9be0df94e3d92b877ad82d995f6e4f55
Kelvin Ubaechu <kubaechu@gmail.com>
2022-05-30 20:04:56 -0400

    Implement removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...toBeRemoved) {
+    for(let i = arr.length - 1; i >= 0; i--) {
+        if(toBeRemoved.includes(arr[i])) {
+            arr.splice(i, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
