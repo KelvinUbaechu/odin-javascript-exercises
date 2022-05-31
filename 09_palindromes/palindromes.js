84354324a9aed49e7811214869253e366ebcb32e
Kelvin Ubaechu <kubaechu@gmail.com>
2022-05-30 21:41:29 -0400

    Implement palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let alphaChars = string.split('').filter((c) => {
+        return c !== ' ' && c.toUpperCase() !== c.toLowerCase();
+    });
+    let midIdx = Math.floor(alphaChars.length / 2);
+    let endIdx = alphaChars.length - 1;
+    for(let i = 0; i < midIdx; i++) {
+        if(alphaChars[i] !== alphaChars[endIdx - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
