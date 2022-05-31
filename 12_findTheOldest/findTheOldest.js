54e7b969beb608855c0bf69e8884acd774bf0e02
Kelvin Ubaechu <kubaechu@gmail.com>
2022-05-31 11:31:34 -0400

    Implement findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
+function getAge(person) {
+    let yearOfDeath = person.yearOfDeath || new Date().getFullYear();
+    return yearOfDeath - person.yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((p1, p2) => getAge(p1) > getAge(p2) ? p1 : p2);
 };
 
 // Do not edit below this line
