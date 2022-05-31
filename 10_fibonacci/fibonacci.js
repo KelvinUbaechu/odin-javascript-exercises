fe15e35615b4e61767379fd5651341256ec85103
Kelvin Ubaechu <kubaechu@gmail.com>
2022-05-30 22:02:42 -0400

    Implement fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if(index <= 0) return 'OOPS';
+    let cur = 0;
+    let next = 1;
+    for(let i = 0; i < index; i++) {
+        let temp = cur;
+        cur = next;
+        next = temp + next;
+    }
+    return cur;
 };
 
 // Do not edit below this line
